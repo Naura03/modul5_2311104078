83740911c16b373ac20fc0dac312536f76c98740
Naura03 <nauraaisha123@gmail.com>
2025-03-20 19:59:37 +0700

    Menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_2311104078/Program.cs
+++ modul5_2311104078/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_2311104078;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 23; // Dua digit dari NIM
+        int num2 = 11;
+        int num3 = 04;
+
+        int hasil = Penjumlahan.JumlahTigaAngka(num1, num2, num3);
+        Console.WriteLine($"Hasil Penjumlahan: {hasil}");
+    }
+}
